29e4437af44e4ccbf241a321fd920f3c1a5b6961
Artsiom Artsiomau <artem.artsemov@biggiko.com>
2021-02-24 10:17:46 +0300

    completly lesson #23

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/main.js', 'public/js')
+mix.js(['resources/js/app.js','resources/js/main.js'], 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
