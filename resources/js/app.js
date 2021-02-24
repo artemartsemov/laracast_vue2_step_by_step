c2519527070b993622a4401fed736e579263a1f4
Artsiom Artsiomau <artem.artsemov@biggiko.com>
2021-02-24 09:02:58 +0300

    completely lesson #21

--- resources/js/app.js
+++ resources/js/app.js
@@ -67,18 +67,29 @@ class Form {
     }
 
     submit(requestType, url ){
-        axios[requestType](url, this.data())
-        .then(this.onSuccess.bind(this))
-        .catch(this.onFail.bind(this));
+        return new Promise((resolve,reject) => {
+
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+
+                    resolve(response.data);
+                })
+                .catch(error => {
+                  this.onFail(error.responce.data);
+
+                  reject(error.responce.data)
+                })
+        });
     }
 
-    onSuccess(response){
-        alert(response.data.message);
+    onSuccess(data){
+        alert(data.message);
         this.errors.clear();
     }
 
     onFail(error){
-         this.errors.record(error.response.data.errors)
+         this.errors.record(error)
     }
 
 }
@@ -97,7 +108,9 @@ const app = new Vue({
 
     methods: {
         onSubmit(){
-                this.form.submit('post', '/projects');
+                this.form.submit('post', '/projects')
+                .then(data => console.log(data))
+                .catch(errors => console.log(errors))
         }
     }
 });
